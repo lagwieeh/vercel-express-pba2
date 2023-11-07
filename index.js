f7f5aadee3096181eeebb0f0f4e05c3634959b30
Eduardo <eduardo.bautista@inacapmail.cl>
2023-11-07 00:06:01 -0300

    ola100

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.static('public'));
 
 //requiring our router to be connected to app
 // app.use(router);
-app.use('/expressions', expressionsRouter);
-app.use('/animals', animalsRouter)
+app.use('/routes/expressions', expressionsRouter);
+app.use('/routes/animals', animalsRouter)
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
